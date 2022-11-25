ea7adbaee5e35762d4e6a9b5de2cbcbe2b8fc847
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-11-25 14:50:23 +0900

    day01
    버튼클릭시 이벤트 추가할것(버튼 변경, 벌레와 당근이미지 랜덤생성)

---
+++ main.js
@@ -0,0 +1,38 @@
+const playBtn = document.querySelector('.play__btn');
+const timer = document.querySelector('.play__timer');
+const countCarrot = document.querySelector('.carrot__num');
+const stopBtn = document.querySelector('.stop__btn');
+
+playBtn.addEventListener('click', () => {
+    startTimer();
+    changeBtn();
+});
+
+
+// Timer
+let time = 60000;
+let min = 1;
+let sec = 60;
+function startTimer (){
+    playTime =setInterval(function () {
+        time = time - 1000;
+        min = time/(60*1000);
+
+        if(sec > 0){
+            sec = sec-1;
+            timer.value=Math.floor(min)+ ':' + sec;
+        }
+        if(sec ===0) {
+            sec=60;
+            timer.value = Math.floor(min) + ':' + '00';
+        }
+    }, 1000);
+}
+
+// changeBtn
+function changeBtn(){
+    if(stopBtn.style.display === 'none'){
+        stopBtn.style.display = 'block';
+    }
+}
+
