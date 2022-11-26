9aec62fc1a95233a8ffac1325a454d697b545241
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-11-26 16:37:03 +0900

    img random (Xbox)

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const field = document.querySelector('.game__filed');
+const fieldRect = field.getBoundingClientRect();
 const playBtn = document.querySelector('.play__btn');
 const timer = document.querySelector('.play__timer');
 const countCarrot = document.querySelector('.carrot__num');
@@ -27,6 +29,36 @@ function startTimer (){
         }
     }, 1000);
 }
+
+function initGame(){
+    console.log(fieldRect);
+    addItem('carrot', 5 , 'img/carrot/png');
+    addItem('bug', 5 , 'img/bug/png');
+}
+
+function addItem(className, count, imgPath){
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width;
+    const y2 = fieldRect.height;
+    for (let i = 0; i < count; i++) {
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute';
+        const x = randomNumber(x1, x2);
+        const y = randomNumber(y1, y2);
+        item.style.left =`${x}px`;
+        item.style.top = `${y}px`
+        field.appendChild(item);
+    }
+}
+
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+initGame();
 
 
 
