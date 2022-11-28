0c2bbc18e77224bf5f3675539d7685f186e62f49
Lee hyemi <107706338+hammimo@users.noreply.github.com>
2022-11-28 11:29:01 +0900

    html/css/js PlayButton ClickEvent

--- main.js
+++ main.js
@@ -1,40 +1,57 @@
 const field = document.querySelector('.game__filed');
 const fieldRect = field.getBoundingClientRect();
 const CarrotSize = 80;
+const CarrotCount = 5;
+const BugCount = 5;
 const playBtn = document.querySelector('.play__btn');
 const timer = document.querySelector('.play__timer');
-const countCarrot = document.querySelector('.carrot__num');
-const stopBtn = document.querySelector('.stop__btn');
+const gameScore = document.querySelector('.game__score');
+
+
+// 게임의 상태을 기억하는 부분
+let started = false; // 게임이 시작되었는지 안되었는지
+let score = 0; //최종적인 점수
 
 playBtn.addEventListener('click', () => {
-    startTimer();
+    if(started){
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
 });
 
 
-// Timer
-let time = 60000;
-let min = 1;
-let sec = 60;
-function startTimer (){
-    playTime =setInterval(function () {
-        time = time - 1000;
-        min = time/(60*1000);
+function startGame(){
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+};
 
-        if(sec > 0){
-            sec = sec-1;
-            timer.value=Math.floor(min)+ ':' + sec;
-        }
-        if(sec ===0) {
-            sec=60;
-            timer.value = Math.floor(min) + ':' + '00';
-        }
-    }, 1000);
+function startGameTimer(){
+
+}; 
+
+function stopGame(){
+
+};
+function showStopButton(){
+    const icon = playBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore(){
+    timer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
 }
 
 function initGame(){
-    console.log(fieldRect);
-    addItem('carrot', 5 , 'img/carrot.png');
-    addItem('bug', 5 , 'img/bug.png');
+    field.innerHTML = ' '; //새로 시작할때마다 텅텅빈 상태에서 아이템 생성
+    gameScore.innerText = CarrotCount;
+    addItem('carrot', CarrotCount , 'img/carrot.png');
+    addItem('bug', BugCount , 'img/bug.png');
 }
 
 function addItem(className, count,imgPath){
@@ -59,7 +76,11 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-initGame();
+function removeItem(){
+
+}
+
+
 
 
 
